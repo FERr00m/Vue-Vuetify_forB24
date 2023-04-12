6f1eecba17755641b767186c3b432a2197fc7c06
FERr00m <59802905+FERr00m@users.noreply.github.com>
2023-04-12 18:15:31 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
 import store from "./store/index.js";
 
 // Vuetify
@@ -8,15 +7,23 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import { aliases, mdi } from 'vuetify/iconsets/mdi-svg';
 
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi', // This is already the default value - only for display purposes
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+
 });
 // Assets
 import "./assets/normalize.css";
 import "./assets/main.css";
-import "./assets/materialdesignicons.min.css";
 
 const app = createApp(App);
 
